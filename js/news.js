26b5016e70015c9474b02d206ef8a15c6efe8bbe
王昊 <wanghao5013@foxmail.com>
2024-09-24 15:54:33 +0800

    页面导航栏和页脚公共数据提取，避免冗余

--- js/news.js
+++ js/news.js
@@ -1,15 +1,18 @@
-const {createApp, ref} = Vue
+const {createApp, ref, onMounted} = Vue
 
 createApp({
     setup() {
-        const navbarData = [
-            {title: '首页', href: 'index.html'},
-            {title: '名师风采', href: 'teacher.html'},
-            {title: '校园生活', href: 'life.html'},
-            {title: '新闻聚焦', href: 'news.html'},
-            {title: '优美风景', href: 'sight.html'},
-            {title: '学校介绍', href: 'school.html'}
-        ];
+        const navbarData = ref([]);
+        const items = ref([]);
+        const footerItems = ref({})
+        const navbarIndex = ref()
+        onMounted(async () => {
+            const data = await (await fetch("./js/data.json")).json();
+            navbarData.value = data.navbarData
+            items.value = data.items
+            footerItems.value = data.footerItems
+            navbarIndex.value = findNavItemIndex(navbarData.value);
+        })
         function findNavItemIndex(navbarData) {
             // 获取当前URL的路径部分，并获取第三个元素（即文件名）
             const currentPath = window.location.pathname.split('/')[2];
@@ -24,40 +27,7 @@ createApp({
             // 如果没有找到匹配的元素，返回0
             return 0;
         }
-        const navbarIndex = findNavItemIndex(navbarData);
 
-        const items = [
-            {
-                class: "col-sm-4 col-lg-2 order-lg-2",
-                title: "关于我们",
-                links: [
-                    {label: "网站声明"},
-                    {label: "学校介绍", url: "school.html"}
-                ]
-            },
-            {
-                class: "col-sm-4 col-lg-2 order-lg-3",
-                title: "特别致谢",
-                links: [
-                    {label: "Bootstrap", url: "https://www.bootcss.com/"},
-                    {label: "jQuery", url: "https://jquery.com/"}
-                ]
-            },
-            {
-                class: "col-sm-4 col-lg-2 order-lg-4",
-                title: "更多链接",
-                links: [
-                    {label: "法律顾问"},
-                    {label: "人才招聘"}
-                ]
-            }
-        ];
-        const footerItems = {
-            name: "University",
-            copyright: "© 2023-2024 School. All Rights Reserved.",
-            icp: "豫ICP备2023005799号",
-            text: "学校以殷殷实力为莘莘学子成就未来，搭建坚实平台，构筑成功之路，让每一位学子走向全国、走向世界、走向成功人生。",
-        };
         const cards = [
             {
                 img: "img/political01.webp",
